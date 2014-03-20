7f7650117fcc635bbefb43967fb2d94d33a309f9
Toàn Nguyễn <nguyenductoan092@gmail.com>
2014-03-20 17:14:57 +0700

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello from <a href="http://appfog.com">AppFog.com</a>');
-}).listen(process.env.VMC_APP_PORT || 1337, null);
